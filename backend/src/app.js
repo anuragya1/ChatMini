87797d12966123dc244d28e5fed617680a35d53e
anurag4ya1 <anuragProf3160@gmail.com>
2025-09-26 03:41:04 +0530

    feat: added messages api endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from './routes/messageRoutes.js'
 import { errorHandler } from "./middlewares/ErrorHandler.js";
 
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-
+app.use('/api/messages', messageRoutes);
 
 app.use(errorHandler);
 export default app;
