f30952010db4834cef236b5f98bac7e4c69b8737
anurag4ya1 <anuragProf3160@gmail.com>
2025-09-26 03:39:54 +0530

    feat: added message limiter 60/min

--- backend/src/middlewares/RateLimit.js
+++ backend/src/middlewares/RateLimit.js
@@ -1,6 +1,6 @@
 import rateLimit from 'express-rate-limit';
 
-// Generic factory for auth limiters
+// Generic factory for limiters
 const createAuthLimiter = (maxAttempts, action = 'auth') => rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: maxAttempts,
@@ -10,9 +10,10 @@ const createAuthLimiter = (maxAttempts, action = 'auth') => rateLimit({
   standardHeaders: true,
   legacyHeaders: false,
   // Skip localhost for development
-//   skip: (req) => req.ip === '::1' || req.ip === '127.0.0.1'
+   skip: (req) => req.ip === '::1' || req.ip === '127.0.0.1'
 });
 
-// Specific limiters for auth endpoints
+// Specific limiters for  endpoints
 export const registerLimiter = createAuthLimiter(10, 'registration');
 export const loginLimiter = createAuthLimiter(5, 'login');
+export const messageLimiter = createAuthLimiter(60,'messages');
